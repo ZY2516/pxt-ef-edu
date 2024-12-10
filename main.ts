2abd872c89540d4b1bdd72d1f5c7f04d12536f70
DeXin64 <129580034+DeXin64@users.noreply.github.com>
2024-12-10 10:43:23 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,4 +3,30 @@ namespace efEduLed {
    export function test():number{
       return 1;
   }
+
+  const MAX_BRIGHTNESS = 10
+
+  export class efEduLedClient extends modules.LedClient {
+
+    constructor(role: string) {
+      super(role)
+    }
+
+    //% block
+    setBrightness(value: number): void {
+      this.setBrightness(value / MAX_BRIGHTNESS)
+    }
+
+    //% block
+    brightness(): number {
+      return this.brightness()
+    }
+
+    //% block
+    rotate(offset = 1): void { this.rotate(offset) }
+
+    //% block
+    shift(offset = 1): void { this.shift(offset) }
+
+  }
 }
