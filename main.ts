b7e10df977bb347e30594c1cb033c8979607cb40
DeXin64 <129580034+DeXin64@users.noreply.github.com>
2024-12-10 16:28:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -40,29 +40,5 @@ namespace ef{
   //% fixedInstance whenUsed weight=1 block="dial1"
   export const dial1 = new FwdEduDialClient("dial1")
 
-    const MAX_BRIGHTNESS = 10
 
-  export class FwdEduLEDClient extends modules.LedClient {
-
-    constructor(role: string) {
-      super(role)
-    }
-
-    //% block
-    setBrightness(value: number): void {
-      this.setBrightness(value / MAX_BRIGHTNESS)
-    }
-
-    //% block
-    brightness(): number {
-      return this.brightness()
-    }
-
-    //% block
-    rotate(offset = 1): void { this.rotate(offset) }
-
-    //% block
-    shift(offset = 1): void { this.shift(offset) }
-
-  }
 }
