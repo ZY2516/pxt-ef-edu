5e3ce843bda828ad57d3d134b16ff31154225070
zhuyu <zhuyu@elecfreaks.com>
2024-12-13 15:00:41 +0800

    asdfasfd

--- main.ts
+++ main.ts
@@ -580,12 +580,6 @@ namespace ef {
 
   //% fixedInstance whenUsed weight=1 block="servo1"
   export const servo1 = new ServoClient("servo1")
-  //% fixedInstance whenUsed weight=2 block="servo2"
-  export const servo2 = new ServoClient("servo2")
-  //% fixedInstance whenUsed weight=3 block="servo3"
-  export const servo3 = new ServoClient("servo3")
-  //% fixedInstance whenUsed weight=4 block="servo4"
-  export const servo4 = new ServoClient("servo4")
 
   //relay
   /**
